0626b1662de8276286117526eacdda2eeab882a8
piglovesyou <thepiglovesyou@gmail.com>
2019-11-12 00:16:14 +0900

    Test `yarn start` and `yarn build`

--- tools/build.test.ts
+++ tools/build.test.ts
@@ -1,27 +1,5 @@
 import path from 'path';
-import waitOn from 'wait-on';
-import fetch from 'node-fetch';
-import assert from 'assert';
-import { execa, killApp, success, timeoutBase } from './lib/test-fns';
-
-async function verifyApp(port: number) {
-  const expected = 'React Starter Kit - www.reactstarterkit.com';
-  const url = `http://localhost:${port}`;
-  await waitOn({
-    resources: [url],
-    timeout: timeoutBase,
-  });
-  const text = await fetch(url).then(r => r.text());
-  const match = text.match(/<title.*?>(.+?)</);
-  if (!match) throw new Error('Title text does not exist');
-
-  const [, actual] = match;
-  assert.strictEqual(actual, expected);
-  success(`Verified app of port ${port}`);
-}
-
-const buildApp = (cwd: string) =>
-  execa('yarn', ['build', '--release'], { cwd });
+import { execa, killApp, timeoutBase, verifyApp } from './lib/test-fns';
 
 const startApp = (cwd: string, port: number) =>
   execa('node', ['build/server.js'], {
@@ -35,7 +13,7 @@ describe('yarn build', () => {
     async () => {
       const port = 3033;
       const cwd = path.resolve(__dirname, '..');
-      await buildApp(cwd);
+      execa('yarn', ['build', '--release'], { cwd });
       const app = startApp(cwd, port);
       await verifyApp(port);
       await killApp(app);
